ba9dee996e1091abe1c985d45eed52b48104db16
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-06-16 22:54:30 +0300

    chore: morgan middleware and user module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(morgan('combined'));
+
+  await app.listen(process.env.PORT!);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+}).catch((error) => {
+  console.error('Error starting the server:', error);
+});
