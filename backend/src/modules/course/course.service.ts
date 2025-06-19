49093472921bb811d50f444d47b8535afd0f205c
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-06-19 12:52:46 +0300

    feat: course module

---
+++ backend/src/modules/course/course.service.ts
@@ -0,0 +1,31 @@
+
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from 'src/models/course.entity';
+
+@Injectable()
+export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
+  ) {}
+
+  async findAll(): Promise<Course[]> {
+    return this.courseRepository.find();
+  }
+
+  async createCourse(course: Course): Promise<Course> {
+    const newCourse = new Course();
+    newCourse.title = course.title;
+    newCourse.description = course.description;
+    newCourse.isPublished = course.isPublished;
+    newCourse.language = course.language;
+    newCourse.imageDeleteURL = course.imageDeleteURL;
+    newCourse.ratingSum = course.ratingSum;
+    newCourse.level = course.level;
+    newCourse.price = course.price;
+    newCourse.numberOfReviewers = course.numberOfReviewers;
+    return this.courseRepository.save(newCourse);
+  }
+}
