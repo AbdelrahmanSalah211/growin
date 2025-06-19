e98a07884c7f79e6c0d5eae2430947a7d03d1228
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-06-19 12:53:54 +0300

    feat: user database entity

---
+++ backend/src/models/user.entity.ts
@@ -0,0 +1,105 @@
+import crypto from 'crypto';
+import * as argon2 from 'argon2';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  EventSubscriber,
+  EntitySubscriberInterface,
+  UpdateEvent,
+  InsertEvent,
+} from 'typeorm';
+
+enum UserMode {
+  LEARNER = 'learner',
+  INSTRUCTOR = 'instructor',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  profileImage: string;
+
+  @Column()
+  imageDeleteURL: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserMode,
+    default: UserMode.LEARNER,
+  })
+  userMode: UserMode;
+
+  @Column()
+  bio: string;
+
+  @Column({ nullable: true })
+  passwordResetToken: string;
+
+  @Column({ nullable: true })
+  passwordResetExpires: Date;
+
+  @BeforeInsert()
+  beforeInsertLowercase() {
+    this.username = this.username.toLowerCase();
+    this.email = this.email.toLowerCase();
+  }
+
+  static async correctPassword(
+    userPassword: string,
+    candidatePassword: string,
+  ): Promise<boolean> {
+    return argon2.verify(userPassword, candidatePassword);
+  }
+
+  createPasswordResetToken(): string {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+    console.log({ resetToken }, this.passwordResetToken);
+    this.passwordResetExpires = new Date(Date.now() + 10 * 60 * 1000);
+    return resetToken;
+  }
+}
+
+@EventSubscriber()
+export class UserSubscriber implements EntitySubscriberInterface<User> {
+  listenTo() {
+    return User;
+  }
+
+  async beforeInsert(event: InsertEvent<User>) {
+    event.entity.password = await argon2.hash(event.entity.password);
+  }
+
+  async beforeUpdate(event: UpdateEvent<User>) {
+
+    const newPassword: string | undefined = (event.entity && typeof event.entity.password === 'string')
+      ? event.entity.password
+      : undefined;
+    const oldPassword: string | undefined = (event.databaseEntity && typeof event.databaseEntity.password === 'string')
+      ? event.databaseEntity.password
+      : undefined;
+
+    if (newPassword && newPassword !== oldPassword && event.entity) {
+      event.entity.password = await argon2.hash(newPassword);
+    }
+  }
+}
+
+export default User;
