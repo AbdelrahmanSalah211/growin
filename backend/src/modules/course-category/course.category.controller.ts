5c44508297ea0802cb887b58f625545e29aa472c
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-05 02:43:47 +0300

    rename route from course-categories to courseCategories

--- backend/src/modules/course-category/course.category.controller.ts
+++ backend/src/modules/course-category/course.category.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CourseCategoryService } from './course.category.service';
 import { CourseCategory } from 'src/models/courses-category.entity';
 
-@Controller('course-categories')
+@Controller('courseCategories')
 export class CourseCategoryController {
   constructor(private readonly categoryService: CourseCategoryService) {}
 
