172491ad18cd3a5e348ef784481a3c25a069bb38
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-09 02:07:14 +0300

    chore: specifing the error message for frontend interceptors

--- backend/src/modules/auth/auth.guard.ts
+++ backend/src/modules/auth/auth.guard.ts
@@ -5,19 +5,19 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { Request } from 'express';
 import { UserMode } from 'src/models';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class JwtAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request & { user?: any }>();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('access_token_missing');
     }
     try {
       const payload: { sub: string; email: string, userMode: UserMode } = await this.jwtService.verifyAsync(
@@ -30,8 +30,11 @@ export class AuthGuard implements CanActivate {
       console.log('JWT payload:', payload);
 
       request.user = payload;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (error){
+      if(error instanceof TokenExpiredError) {
+        throw new UnauthorizedException('access_token_expired');
+      }
+      throw new UnauthorizedException('access_token_invalid');
     }
     return true;
   }
