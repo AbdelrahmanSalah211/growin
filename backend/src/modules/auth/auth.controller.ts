e1d9bd6340a3b40635cf2b8f47cf18dbc8394112
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-09 02:06:19 +0300

    feat: refresh token and logut

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -4,12 +4,16 @@ import {
   Body,
   Get,
   Req,
+  Res,
   UseGuards,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { CreateUserDto } from '../user/dto/user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './auth.guard';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -18,33 +22,83 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  async login(@Body() dto: SignInDto) {
-    return this.authService.signIn(dto);
+  async login(
+    @Body() dto: SignInDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { user, accessToken, refreshToken } = await this.authService.signIn(dto);
+    response.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+    return {
+      message: 'Login Successful',
+      accessToken,
+      user
+    };
   }
 
   @Post('signup')
   async createUser(
+    @Res({ passthrough: true }) response: Response,
     @Body() dto: CreateUserDto,
-  ): Promise<{ accessToken: string }> {
-    const { accessToken } = await this.authService.signUp(dto);
-    return { accessToken };
+  ): Promise<{ message: string }> {
+    return await this.authService.signUp(dto);
+  }
+
+  @Post('refresh')
+  async refreshAccessToken(
+    @Req() req: Request,
+  ) {
+    const refreshToken = req.cookies?.refreshToken;
+
+    if(!refreshToken){
+      throw new UnauthorizedException('Refresh token not found');
+    }
+
+    const { accessToken } = await this.authService.refreshAccessToken({ refreshToken });
+
+    return { 
+      accessToken,
+      message: 'Access token refreshed successfully'
+    };
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logout(
+    @Req() req: { user: { sub: number } },
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.logout(req.user.sub)
+    response.clearCookie('refreshToken');
+    return { message: 'Logged out successfully' };
   }
 
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleLogin() {}
-  d;
+  // d;
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleCallback(@Req() req: Request & { user: { email: string; name: string; photo: string } }) {
-    const user = req.user;
-    console.log('Google user:', user);
-    const jwt = await this.authService.loginWithOAuth(user);
+  async googleCallback(
+    @Req() req: Request & { user: { email: string; name: string; photo: string } },
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { user, accessToken, refreshToken } = await this.authService.loginWithOAuth(req.user);
+    response.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
     return {
       message: 'Google authentication successful',
       user,
-      accessToken: jwt.accessToken,
+      accessToken
     };
   }
 }
