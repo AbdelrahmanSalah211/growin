2f10451d41eb01b95e02ecb618fc758a10e706e3
AbdelrahmanEmbaby <73141302+AbdelrahmanEmbaby@users.noreply.github.com>
2025-07-13 07:05:34 +0300

    style(frontend): update global styles and layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { FormContainer } from "@/components/layout/FormContainer";
+import { getCourses } from "@/services/courseService";
+import { getToken } from "@/lib/auth-actions";
+import { useHydrateAuth } from "@/hooks/useHydrateAuth";
+import { useAuthStore } from "@/stores/authStore";
+import { useEffect } from "react";
 
 export default function Home() {
+  useHydrateAuth();
+  const accessToken = useAuthStore((state) => state.token) || "";
+
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      if (!accessToken) return;
+      try {
+        const courses = await getCourses(accessToken);
+        setCourses(courses);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchCourses();
+  }, [accessToken]); // re-run whenever token changes
+
   return (
-    <div className="bg-background">
-      safafadasda
+    <div>
+      <FormContainer />
+      <ul>
+        {courses.map((course: any) => (
+          <li key={course.id}>{course.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
