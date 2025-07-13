467b82e2c9846182d28fca62dec668ce4b1313dd
AbdelrahmanEmbaby <73141302+AbdelrahmanEmbaby@users.noreply.github.com>
2025-07-13 07:06:41 +0300

    feat(frontend): update course service logic

--- frontend/src/services/courseService.ts
+++ frontend/src/services/courseService.ts
@@ -1,40 +1,46 @@
 import axios from "axios";
 import { AxiosError } from "axios";
 
+import { getAccessToken } from "@/stores/authStore";
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const accessToken = null; //TODO attach access token from context
 
 interface createCoursePayload {
-    title: string;
-    description: string;
-    isPublished: boolean;
-    language: string;
-    imageDeleteURL: string;
-    ratingSum: number;
-    level: "beginner" | "intermediate" | "advanced";
-    price: number;
-    numberOfReviewers: number;
+  title: string;
+  description: string;
+  isPublished: boolean;
+  language: string;
+  imageDeleteURL: string;
+  ratingSum: number;
+  level: "beginner" | "intermediate" | "advanced";
+  price: number;
+  numberOfReviewers: number;
 }
-async function getCourses() {
-    try {
-        const response = await axios.get(`${API_URL}/courses`);
-        return response.data;
-    } catch (error) {
-        const axiosError = error as AxiosError;
-        throw axiosError.response?.data || axiosError.message;
-    }
+export async function getCourses(accessToken: string) {
+  try {
+    const response = await axios.get(`${API_URL}/courses`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    throw axiosError.response?.data || axiosError.message;
+  }
 }
 
 async function createCourse(payload: createCoursePayload) {
-    try {
-        const response = await axios.post(`${API_URL}/courses`, payload, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-        return response.data;
-    } catch (error) {
-        const axiosError = error as AxiosError;
-        throw axiosError.response?.data || axiosError.message;
-    }
+  const accessToken = getAccessToken();
+  try {
+    const response = await axios.post(`${API_URL}/courses`, payload, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    throw axiosError.response?.data || axiosError.message;
+  }
 }
