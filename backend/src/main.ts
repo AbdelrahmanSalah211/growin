cdce9e9d9ef7c00d4e2bdebb351fd80fa841e45a
Ramy Elbasha <ramyelbasha2@gmail.com>
2025-07-14 06:32:25 +0300

    fixing the conflict

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,6 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.FRONTEND_URL, credentials: true });
-
   app.use(morgan('combined'));
   app.use(cookieParser());
 
