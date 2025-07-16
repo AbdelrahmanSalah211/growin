b6c2ec06ea13b04d60acd42c799ce95a29479318
Abdelrahmansherif207 <abdelrahmansherif207@gmail.com>
2025-07-16 16:07:00 +0300

    update: frontend/services

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -4,153 +4,154 @@ import axiosInstance from "@/lib/axiosInstance";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export interface updateInfoPayload {
-  username?: string;
-  email?: string;
-  profileImage?: string;
-  imageDeleteURL?: string;
-  bio?: string;
+    username?: string;
+    email?: string;
+    profileImage?: string;
+    imageDeleteURL?: string;
+    isPasswordPresent?: boolean;
+    bio?: string;
 }
 
 export interface updatePassword {
-  currentPassword: string;
-  newPassword: string;
+    currentPassword: string;
+    newPassword: string;
 }
 
 export async function getUserInfo(accessToken: string) {
-  try {
-    const response = await axiosInstance.get(`${API_URL}/users/info`, {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    try {
+        const response = await axiosInstance.get(`${API_URL}/users/info`, {
+            withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function updateUserInfo(
-  payload: updateInfoPayload,
-  accessToken: string
+    payload: updateInfoPayload,
+    accessToken: string
 ) {
-  try {
-    const response = await axiosInstance.patch(
-      `${API_URL}/users/info`,
-      payload,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    try {
+        const response = await axiosInstance.patch(
+            `${API_URL}/users/info`,
+            payload,
+            {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function updatePassword(
-  payload: updatePassword,
-  accessToken: string
+    payload: updatePassword,
+    accessToken: string
 ) {
-  try {
-    const response = await axiosInstance.patch(
-      `${API_URL}/users/password`,
-      payload,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    try {
+        const response = await axiosInstance.patch(
+            `${API_URL}/users/password`,
+            payload,
+            {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function createPassword(
-  payload: { password: string },
-  accessToken: string
+    payload: { password: string },
+    accessToken: string
 ) {
-  if (!payload.password) {
-    throw new Error("Password is required");
-  }
-  try {
-    const response = await axiosInstance.patch(
-      `${API_URL}/users/create-password`,
-      payload,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    if (!payload.password) {
+        throw new Error("Password is required");
+    }
+    try {
+        const response = await axiosInstance.patch(
+            `${API_URL}/users/create-password`,
+            payload,
+            {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function forgetPassword(email: string) {
-  try {
-    const response = await axiosInstance.post(
-      `${API_URL}/users/forget-password`,
-      { email },
-      {
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    try {
+        const response = await axiosInstance.post(
+            `${API_URL}/users/forget-password`,
+            { email },
+            {
+                withCredentials: true,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function resetPassword(
-  token: string,
-  newPassword: string
+    token: string,
+    newPassword: string
 ): Promise<{ message: string }> {
-  try {
-    const response = await axiosInstance.patch(
-      `${API_URL}/users/reset-password`,
-      { token, newPassword },
-      {
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    try {
+        const response = await axiosInstance.patch(
+            `${API_URL}/users/reset-password`,
+            { token, newPassword },
+            {
+                withCredentials: true,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
 
 export async function switchUserMode(accessToken: string) {
-  try {
-    const response = await axiosInstance.patch(
-      `${API_URL}/users/user-mode`,
-      {},
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError.response?.data || axiosError.message;
-  }
+    try {
+        const response = await axiosInstance.patch(
+            `${API_URL}/users/user-mode`,
+            {},
+            {
+                withCredentials: true,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        const axiosError = error as AxiosError;
+        throw axiosError.response?.data || axiosError.message;
+    }
 }
