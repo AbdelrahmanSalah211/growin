3defcb9ae10c56cceefa6638bd43202fb3aa3e32
AbdelrahmanEmbaby <73141302+AbdelrahmanEmbaby@users.noreply.github.com>
2025-07-18 00:03:52 +0300

    fix: update button width to use responsive class

--- frontend/src/components/ui/buttons/button.tsx
+++ frontend/src/components/ui/buttons/button.tsx
@@ -19,7 +19,7 @@ export const Button: FC<ButtonProps> = ({
   children = "Button",
 }) => {
   const defaultStyle =
-    "cursor-pointer w-[23.125rem] py-[0.9375rem] rounded-[0.625rem] text-[1.125rem] font-bold text-primary-text bg-background hover:text-background hover:bg-primary-text transition-colors disabled:text-secondary-text disabled:bg-background disabled:cursor-not-allowed";
+    "cursor-pointer w-md py-[0.9375rem] rounded-[0.625rem] text-[1.125rem] font-bold text-primary-text bg-background hover:text-background hover:bg-primary-text transition-colors disabled:text-secondary-text disabled:bg-background disabled:cursor-not-allowed";
 
   return (
     <button
